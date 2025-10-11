1a5f44f85c24d101d83b9c16ab4119574d3b1afc
caoliu <l-cao20@mails.tsinghua.edu.cn>
2025-10-12 06:07:31 +0800

    feature: graph neural network

--- model.py
+++ model.py
@@ -3,6 +3,46 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from torch_geometric.nn import GCNConv
+
+SMPL_J24_NUM_NODES = 24
+SMPL_J24_EDGES = [
+    (0, 1), (1, 4), (4, 7), (7, 10),    # left leg
+    (0, 2), (2, 5), (5, 8), (8, 11),    # right leg
+    (0, 3), (3, 6), (6, 9),             # spine
+    (9, 12), (12, 15),                  # head
+    (9, 13), (13, 16), (16, 18), (18, 20), (20, 22), # left arm
+    (9, 14), (14, 17), (17, 19), (19, 21), (21, 23), # right arm
+]
+
+class SpatialGNN(nn.Module):
+    def __init__(self, in_dim: int = 3, out_dim: int = 64):
+        super().__init__()
+        self.num_nodes = SMPL_J24_NUM_NODES
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.edges = torch.tensor(SMPL_J24_EDGES, dtype=torch.long).t().contiguous()
+        
+        self.gnn1 = GCNConv(in_dim, out_dim * 2)
+        self.gnn2 = GCNConv(out_dim * 2, out_dim)
+
+    def forward(self, joints_xyz: torch.Tensor):
+        '''
+        joints_xyz: [B, T, N, 3]
+        return:
+            node_features: [B, T, N, out_dim]
+        '''
+        B, T, N, _ = joints_xyz.shape
+        x = joints_xyz.reshape(B * T * N, -1)    # [B*T*N, 3]
+        
+        edge_index = self.edges.to(joints_xyz.device)
+        batch = torch.arange(B * T, device=joints_xyz.device).repeat_interleave(N)
+        
+        x = self.gnn1(x, edge_index, batch)
+        x = self.gnn2(x, edge_index, batch)
+        x = x.reshape(B, T, N, -1) # [B, T, N, out_dim]
+        return x
+
 class PositionalEmbedding(nn.Module):
     def __init__(self, d_model: int, max_len: int = 100):
         super().__init__()
@@ -48,7 +88,10 @@ class AutoEncoder(nn.Module):
         self.data_dim = data_dim
         self.latent_dim = latent_dim
 
-        self.input_proj = nn.Linear(data_dim, latent_dim)
+        self.spatial_gnn = SpatialGNN(in_dim=3, out_dim=latent_dim)
+
+        per_frame_dim = 7 + self.spatial_gnn.num_nodes * latent_dim
+        self.input_proj = nn.Linear(per_frame_dim, latent_dim)
         self.pos_embed = PositionalEmbedding(latent_dim, max_len=2048)
         
         encoder_layer = nn.TransformerEncoderLayer(
@@ -69,7 +112,18 @@ class AutoEncoder(nn.Module):
         self.quat_unit_norm = QuatUnitNorm(rot_start=3, rot_dim=4)
 
     def forward(self, x: torch.Tensor):
+        '''
+        x: [B, T, D]
+        return:
+            decoded_sequence: [B, T, D]
+        '''
+        B, T, D = x.shape
 
+        root = x[..., :7]
+        kps = x[..., 7:].reshape(B, T, self.spatial_gnn.num_nodes, -1)
+        kps_feat = self.spatial_gnn(kps)        # [B, T, N, latent_dim]
+
+        x = torch.cat([root, kps_feat.reshape(B, T, -1)], dim=-1)   # [B, T, 7 + N * latent_dim]
         x_proj = self.input_proj(x)
         x_proj = self.pos_embed(x_proj)
 
